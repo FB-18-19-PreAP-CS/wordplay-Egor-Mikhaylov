d405f8c2804bf7a415fbfa223333aa40cb3baa6b
MIKHAYLOV.EGOR <2201136@fbisd.flourbluffschools.net>
2018-11-05 14:06:56 -0600

    complete

--- exercises_1-6.py
+++ exercises_1-6.py
@@ -58,11 +58,108 @@ def count_avoids(forbidden_letters):
                 if avoids(word, forbidden_letters) == True:
                     count += 1
     print(count)
-        
+
+def uses_only(word, letters):
+    '''
+    >>> uses_only("hello", 'hello')
+    True
+    
+    >>> uses_only("hello", 'HELLO')
+    True
+    
+    >>> uses_only("hello", 'HELLOU')
+    True
+    
+    >>> uses_only('jack', 'hello')
+    False
+
+    '''
+    for letter in word:
+        if letter.lower() not in letters.lower():
+            return False
+    return True
+    
+def words_with_only(letters):
+    with open("words.txt") as file:
+        for line in file:
+            for word in line.strip().split():
+                if uses_only(word, letters) == True:
+                    print(word)
+    
+def uses_all(word, letters):
+    '''
+    >>> uses_all('hello', 'hel')
+    True
+    
+    >>> uses_all('hello', 'helLO')
+    True
+    
+    >>> uses_all('hello', 'HelloJ')
+    False
+    
+    >>> uses_all('heLLo', 'HEllo')
+    True
+    '''
+    for letter in letters:
+        if letter.lower() not in word.lower():
+            return False
+    return True
+    
+def how_many_uses_all(letters):
+    count = 0
+    with open("words.txt") as file:
+        for line in file:
+            for word in line.strip().split():
+                if uses_all(word, letters) == True:
+                    count += 1
+    print(count)
+
+def is_abecedarian(word):
+    '''
+    >>> is_abecedarian("Aegilops")
+    True
+    
+    >>> is_abecedarian("abcde")
+    True
+    
+    >>> is_abecedarian('abCdde')
+    True
+    
+    >>> is_abecedarian('abcdzy')
+    False
+    '''
+    old_letter = "a"
+    for letter in word:
+        if letter.lower() >= old_letter.lower():
+            old_letter = letter
+        else:
+            return False
+    return True
+
+def count_abecedarian():
+    count = 0
+    with open("words.txt") as file:
+        for line in file:
+            for word in line.strip().split():
+                if is_abecedarian(word):
+                    count += 1
+    print(count)
+
+
+
 
 if __name__ == "__main__":
     #at_least()
     #no_e()
     #print(avoids("hello", "abcdE"))
     #count_avoids('jxzwq')
+    #print(uses_only("hello", "hEloj"))
+    #words_with_only('catb')
+    #print(uses_all("hello", "hElo"))
+    #how_many_uses_all("heLO")
+    #print(is_abecedarian("Aegilops"))
+    #count_abecedarian()
     pass
+    
+
+    
